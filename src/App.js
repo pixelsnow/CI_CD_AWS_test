53442d052f37db02d74dce6edd8a4a7b7420af37
Valerie <lera.vagapova@gmail.com>
2023-01-23 13:36:31 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Eyvaz, Tuan and Lera were here
         </p>
         <a
           className="App-link"
