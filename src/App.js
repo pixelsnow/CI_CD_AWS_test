4d072d3562ae9ef060d494b4203cc7cb9ce93381
Vagapova Valeriia <lera.vagapova@gmail.com>
2023-01-23 13:47:40 +0200

    clean react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Eyvaz, Tuan and Lera were here
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Eyvaz, Tuan and Lera were here</h1>
+      <p>
+        Failed to deploy via Beanstalk and CodePipeline, but Amplify saved us
+      </p>
     </div>
   );
 }
